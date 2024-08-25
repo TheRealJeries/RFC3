24a4973f18a62f229360f9da84501d9f91d666bb
Jeries <Jeriesd1998@gmail.com>
2024-08-24 20:10:22 -0400

    Added more info fields

--- Scrape_RFCs.py
+++ Scrape_RFCs.py
@@ -7,21 +7,42 @@ import pywebio
 from pywebio.input import input, TEXT
 from pywebio.output import put_text, put_html, put_markdown, put_table
 import Tree_HTML
+import re
+
+more_info_keys=["Obsoleted by", "Obsoletes", "Updated by", "Updates"]
 
 def generate_HTML(RFCS):
-	HTML = Tree_HTML.header_html() 
+	HTML = Tree_HTML.header_html()
 	HTML = HTML + Tree_HTML.start_tree()
 	for RFC in RFCS:
 		if RFC is not {}:
-			HTML = HTML + Tree_HTML.generate_tree_node(RFC["Number"])
+			html_to_paste = RFC["Number"]
+			HTML = HTML + Tree_HTML.generate_tree_node(html_to_paste)
 	HTML = HTML + Tree_HTML.end_tree()
 	HTML = HTML + "</nav>"
 	put_html(HTML)
 
+def regex_to_match_RFCs_by(key_word):
+	return ".*"+key_word+"\s+(RFC \d+\s*(,\s*RFC \d+)*).*"
+
+def add_more_info_to_RFC(RFC, data):
+	for key in more_info_keys:
+		obsoleted_match = re.match(regex_to_match_RFCs_by(key), data)
+		if obsoleted_match:
+			action_RFC_nums = obsoleted_match.group(1).split(",")
+			print(RFC["Number"]+" "+key+":")
+			for action_RFC_num in action_RFC_nums:
+				print("\t"+action_RFC_num)
+		else:
+			RFC[key] = ""
+
 def create_RFC_dictionary_item(datas, headers):
 	RFC = {}
 	for data, header in zip(datas,headers):
-		RFC[header.text] = data.text.replace(u'\xa0', u' ')
+		cleaned_data = data.text.replace(u'\xa0', u' ')
+		if header.text == 'More Info':
+			add_more_info_to_RFC(RFC, cleaned_data)
+		RFC[header.text] = cleaned_data
 	return RFC
 
 def create_RFC_dictionary(argv):
@@ -35,7 +56,7 @@ def create_RFC_dictionary(argv):
 	#search_by_title = argv[1]
 	search_by_title = input("Provide a keyword or title to search by", type=TEXT)
 
-	URL = "http://www.rfc-editor.org/search/rfc_search_detail.php?title={title}".format(title=search_by_title)
+	URL = "http://www.rfc-editor.org/search/rfc_search_detail.php?page=All&title={title}&sortkey=Number&sorting=ASC".format(title=search_by_title)
 	page = requests.get(URL).text
 	soup = BeautifulSoup(page, 'html.parser')
 	table = soup.find("table", "gridtable")
@@ -53,5 +74,4 @@ def create_RFC_dictionary(argv):
 
 if __name__ == '__main__':
 	RFCS = create_RFC_dictionary(sys.argv)
-	print(RFCS)
 	generate_HTML(RFCS)
--- Tree_HTML.py
+++ Tree_HTML.py
@@ -6,9 +6,6 @@ HEADER_HTML = "<link href='https://fonts.googleapis.com/css?family=Fjalla+One' r
 HEADER_HTML = HEADER_HTML + "<meta name='viewport' content='width=device-width, initial-scale=1'>\n"
 HEADER_HTML = HEADER_HTML + "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css'>\n"
 HEADER_HTML = HEADER_HTML + "<link rel='stylesheet' href='https://codepen.io/joellesenne/pen/pdMPdW.css'>"
-HEADER_HTML = HEADER_HTML + "<style>"
-with open('css/tree_style.css') as f: HEADER_HTML = HEADER_HTML + f.read()
-HEADER_HTML = HEADER_HTML + "</style>"
 HEADER_HTML = HEADER_HTML + "<nav class='nav'>"
 
 
